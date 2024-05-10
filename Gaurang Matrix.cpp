60aa22efee055a5b537a0fb2de0b7cc2e5c36f3d
GAURANG SHARMA <146466145+gaurangsharma7654@users.noreply.github.com>
2024-05-11 00:21:06 +0530

    Gaurang Matrix.cpp

--- Gaurang Matrix.cpp
+++ Gaurang Matrix.cpp
@@ -1,64 +1,44 @@
-#include <iostream>
-#include <vector>
+def spiral_order(matrix):
+    if not matrix:
+        return []
 
-using namespace std;
+    result = []
+    top = 0
+    bottom = len(matrix) - 1
+    left = 0
+    right = len(matrix[0]) - 1
 
-vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    int row = matrix.size();
-    int col = matrix[0].size();
-    vector<int> ans;
-    int count = 0;
-    int total = row*col;
-    
-    int startingRow = 0;
-    int startingCol = 0;
-    int endingRow = row-1;
-    int endingCol = col-1;
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
 
-    while(count<total) {
-        for(int index = startingCol;count<total && index<=endingCol;index++) {
-            ans.push_back(matrix[startingRow][index]);
-            count++;
-        }
-        startingRow++;
-        
-        for(int index = startingRow;count<total && index<=endingRow;index++) {
-            ans.push_back(matrix[index][endingCol]);
-            count++;
-        }
-        endingCol--;
-        
-        for(int index = endingCol;count<total && index >= startingCol;index--) {
-            ans.push_back(matrix[endingRow][index]);
-            count++;
-        }
-        endingRow--;
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
 
-        for(int index= endingRow;count<total && index>=startingRow;index--) {
-            ans.push_back(matrix[index][startingCol]);
-            count++;
-        }
-        startingCol++;
-    }
-    return ans;
-}
-int main() {
-    int row,col;
-    cin>>row>>col;
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
 
-    vector<vector<int>> matrix(row,vector<int>(col));
-    
-    cout<<"Enter the elements of matrix"<<endl;
-    for(int i=0;i<row;i++) {
-        for(int j=0;j<col;j++) {
-            cin>>matrix[i][j];
-        }
-    }
-    
-    vector<int> result = spiralOrder(matrix);
-    cout<<"Spiral Matrix"<<endl;
-    for(int i=0;i<result.size();++i) {
-        cout<<result[i]<<" ";
-    }
-    return 0;
-}
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+    return result
+
+# Even matrix
+even_matrix = [[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]]
+
+# Odd matrix
+odd_matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
+
+print("Even matrix spiral order:", spiral_order(even_matrix))
+print("Odd matrix spiral order:", spiral_order(odd_matrix))
